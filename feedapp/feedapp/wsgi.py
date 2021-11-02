3ee3f7979e1b8d104c86111be7782bc98d85d638
Ayoub Tammaoui <44643583+Tammaoui@users.noreply.github.com>
2021-11-02 15:11:40 +0100

    rename prod to settings

--- feedapp/feedapp/wsgi.py
+++ feedapp/feedapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedapp.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedapp.settings')
 
 application = get_wsgi_application()
