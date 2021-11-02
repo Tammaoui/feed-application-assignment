190b04ee3db54edfc6b6f849f50ff0a53e9798c9
Ayoub Tammaoui <44643583+Tammaoui@users.noreply.github.com>
2021-11-02 15:08:32 +0100

    should work

--- feedapp/feedapp/dev.py
+++ feedapp/feedapp/dev.py
@@ -1,14 +1,1 @@
 from .common import *
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
