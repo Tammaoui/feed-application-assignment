71df4e364cac991eec9155c30963a152b6fb5aa4
Ayoub Tammaoui <ayoub.tammaoui@knowit.no>
2021-11-17 16:19:20 +0100

    Demo

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('polls', views.polls),
+    path('hello-world', views.hello_world)
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,28 @@ from http import HTTPStatus
 
 from .models import Poll, Choice
 
+from django.views.decorators.csrf import csrf_exempt
+
+# Lage en poll
+
+# Lage choices til en poll
+
+# Lage profilside
+
+# Lage de forskjellige sidene
+# 1. Lage poll 2. Lag options 3. Stem på poll
+
+@csrf_exempt
+def hello_world(request):
+    if request.method == "POST":
+        print(request.POST['poll_question'])
+    return HttpResponse("Hello world!")
 
 def polls(request):
     response_data = None
     match request.method:
         case "POST":
-            poll_question = request.POST['email']
+            poll_question = request.POST['poll_question']
             created_by = request.user
             poll = Poll(poll_question=poll_question, created_by=created_by)
             poll.save()
@@ -16,11 +32,16 @@ def polls(request):
             poll_id = request.DELETE['id']
             Poll.objects.get(pk=poll_id).delete()
         case "PUT":
-            print("Update poll")
+            pass
         case "GET":
-            response_data = Poll.objects.get()
+            response_data = Poll.objects.get(created_by=request.user)
 
     return HttpResponse(response_data, status=HTTPStatus.OK)
+
+
+def search_for_polls(request, query):
+    poll = Poll.objects.get(poll_question__contains=query)
+    return HttpResponse(poll)
 
 
 def choices(request):
